0557e996f2cf32e38d0b31af2d547248148ef942
doggy-byte <183609412+doggy-byte@users.noreply.github.com>
2024-10-12 15:21:56 +0900

    remove hidden files

--- .ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Unofficial 17Lands Metrics Quiz")
-
-metrics_quiz = st.Page(page="metrics_quiz.py", title="Which is stronger?")
-best_color = st.Page(page="best_color.py", title="Best Deck Color")
-
-pg = st.navigation([metrics_quiz, best_color])
-pg.run()
